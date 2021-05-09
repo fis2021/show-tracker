943cd47317b17b27e74f69f52827bfc2895afc74
llNicoleta <idlnicoleta@gmail.com>
2021-05-09 16:47:51 +0300

    [ST-24] - Added the similar shows button

---
+++ src/main/java/fis/project/st/controllers/SimilarButtonController.java
@@ -0,0 +1,13 @@
+package fis.project.st.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class SimilarButtonController {
+    @FXML
+    public void handleGetSimilar(ActionEvent event) throws IOException {
+        System.out.println("Similar show");
+    }
+}
