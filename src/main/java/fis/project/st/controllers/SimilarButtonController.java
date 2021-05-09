f4df2653df18c11109fc0fdb5c5ee16c24d5e682
llNicoleta <idlnicoleta@gmail.com>
2021-05-09 17:00:42 +0300

    [ST-25] - Retrieved the similar shows data from the API.

--- src/main/java/fis/project/st/controllers/SimilarButtonController.java
+++ src/main/java/fis/project/st/controllers/SimilarButtonController.java
@@ -1,13 +1,26 @@
 package fis.project.st.controllers;
 
+import fis.project.st.model.Show;
+import fis.project.st.requests.requests;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SimilarButtonController {
+    private static ArrayList<Show> similarShows;
+
     @FXML
     public void handleGetSimilar(ActionEvent event) throws IOException {
-        System.out.println("Similar show");
+        if(HomepageController.getSelectedShow().getType().equals("tv")) {
+            requests req = new requests();
+            String responseBody = req.getData("/tv/" + HomepageController.getSelectedShow().getId() + "/similar?", "");
+            similarShows = req.getBaseData(responseBody, "tv");
+        } else {
+            requests req = new requests();
+            String responseBody = req.getData("/movie/" + HomepageController.getSelectedShow().getId() + "/similar?", "");
+            similarShows = req.getBaseData(responseBody, "movie");
+        }
     }
 }
